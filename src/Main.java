a2735729c246baa1861f9ffa59eb16c7e1901f19
Ян <peroff007@gmail.com>
2024-04-01 19:18:01 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.println("Задание 1:");
+        int[] spending = {13000,20000,14000,22500,14500};
+        int sum = 0;
+        for (int i = 0; i < spending.length; i++){
+            sum = sum + spending[i];
         }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        sum = 0;
+        for (int element : spending){
+            sum = sum + element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
 }
