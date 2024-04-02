ba7846a7d6e381b7cf1e32b759ddfad6e8986f6e
Ян <peroff007@gmail.com>
2024-04-02 07:52:22 +0300

    Task 2 and 3 corrected

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1:");
-        int[] spending = {13000,20000,14000,22500,14500};
+        int[] spending = {13000, 20000, 14000, 22500, 14500};
         int sum = 0;
-        for (int i = 0; i < spending.length; i++){
+        for (int i = 0; i < spending.length; i++) {
             sum = sum + spending[i];
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
         sum = 0;
-        for (int element : spending){
+        for (int element : spending) {
             sum = sum + element;
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
@@ -16,12 +16,12 @@ public class Main {
         System.out.println("Задание 2:");
         int spendingMax = spending[0];
         int spendingMin = spending[0];
-        for (int i = 0; i < spending.length; i++){
+        for (int i = 0; i < spending.length; i++) {
             final int current = spending[i];
-            if (current > spendingMax){
+            if (current > spendingMax) {
                 spendingMax = current;
             }
-            if (current < spendingMin){
+            if (current < spendingMin) {
                 spendingMin = current;
             }
         }
@@ -29,15 +29,15 @@ public class Main {
         System.out.println(" ");
         System.out.println("Задание 3:");
         sum = 0;
-        for (int i = 0; i < spending.length; i++){
+        for (int i = 0; i < spending.length; i++) {
             sum = sum + spending[i];
         }
         double result = 1.0 * sum / spending.length;
         System.out.println("Средняя сумма трат за месяц составила " + result + " рублей");
         System.out.println(" ");
         System.out.println("Задание 4:");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length; i > 0; i--){
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length; i > 0; i--) {
             System.out.print(reverseFullName[i - 1]);
         }
     }
